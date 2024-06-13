fd264f97f130f1abd7289e92b0cba0ba8abb9c92
namle <lephuongnam958506@gmail.com>
2024-06-13 10:51:39 +0700

    done header component

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,8 +1,22 @@
-import { Avatar, Box, Flex, Text, VStack } from "@chakra-ui/react"
-import { BsFacebook, BsInstagram } from "react-icons/bs"
+import { Avatar, Box, Flex, Menu, MenuButton, MenuItem, MenuList, Portal, Text, VStack, useToast } from "@chakra-ui/react"
+import { BsInstagram } from "react-icons/bs"
+import { CgMoreO } from "react-icons/cg"
 import { Link } from "react-router-dom"
 
 const UserHeader = () => {
+    const toast = useToast();
+    const copyURL = () => {
+        const currentURL = window.location.href;
+        navigator.clipboard.writeText(currentURL).then(() => {
+            toast({
+                title: 'URL copied.',
+                description: "Profile link copied",
+                status: 'success',
+                duration: 3000,
+                isClosable: true,
+            })
+        });
+    }
     return (
         <VStack gap={4} alignItems={'start'}>
             <Flex justifyContent={"space-between"} w={"full"}>
@@ -22,15 +36,41 @@ const UserHeader = () => {
             </Flex>
 
             <Text>Co-founder, executive chairman and CEO of Meta Platform.</Text>
+
             <Flex justifyContent={'space-between'} w={'full'}>
                 <Flex gap={2} alignItems={'center'}>
                     <Text color={'gray.light'}>3.2M followers</Text>
                     <Box w={1} h={1} bg={'gray.light'} borderRadius={'full'}></Box>
                     <Link style={{ color: '#616161' }}>instagram.com</Link>
                 </Flex>
-                <Flex gap={4} alignItems={'center'}>
-                    <BsInstagram size={24} cursor={'pointer'} />
-                    <BsFacebook size={24} cursor={'pointer'} />
+                <Flex>
+                    <Box className="icon-container">
+                        <BsInstagram size={24} cursor={'pointer'} />
+                    </Box>
+                    <Box className="icon-container">
+                        <Menu>
+                            <MenuButton>
+                                <CgMoreO size={24} cursor={'pointer'} />
+                            </MenuButton>
+                            <Portal>
+                                <MenuList bg={"gray.dark"}>
+                                    <MenuItem bg={"gray.dark"} onClick={copyURL}>Copy link</MenuItem>
+                                </MenuList>
+                            </Portal>
+                        </Menu>
+                    </Box>
+                </Flex>
+            </Flex>
+
+            <Flex w={"full"}>
+                <Flex flex={1} borderBottom={"1.5px solid white"} pb="3" justifyContent={"center"} cursor={"pointer"}>
+                    <Text fontWeight={"bold"}>Threads</Text>
+                </Flex>
+                <Flex flex={1} borderBottom={"1px solid gray"} pb="3" color={"gray.light"} justifyContent={"center"} cursor={"pointer"}>
+                    <Text fontWeight={"bold"}>Replies</Text>
+                </Flex>
+                <Flex flex={1} borderBottom={"1px solid gray"} pb="3" color={"gray.light"} justifyContent={"center"} cursor={"pointer"}>
+                    <Text fontWeight={"bold"}>Reports</Text>
                 </Flex>
             </Flex>
         </VStack>
